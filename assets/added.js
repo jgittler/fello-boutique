59b1b5ac572df0972dfe6acbb39ea4aa477848d8
jgittler <jason.gittler@gmail.com>
2016-09-07 01:33:23 -0700

    performance round 1, collaboration products, subtabs for product page, home page layout v1, photo credits, customer care, update cart total live, policy no header, new img for fellosophy bottom, new favicon, no sticky header

--- assets/added.js
+++ assets/added.js
@@ -4,18 +4,8 @@ function sleep(time) {
 
 $(".quick-view").on("click", function() {
   sleep(500).then(() => {
-    var btn = $("#quick-view-modal").find(".btn-modal").remove();
-    var target = $("#quick-view-modal").find(".modal").remove();
+    $("#quick-view-modal").find("#size-link").remove();
+    $("#quick-view-modal").find(".price").css("padding-bottom", "20px");
+    $(".product-single__thumbnail").removeClass("hidden");
   });
 });
-
-//   $(".quick-view").on("click", function() {
-//     sleep(1500).then(() => {
-//       var btn = $("#quick-view-modal").find(".btn-modal");
-//       var target = $("#quick-view-modal").find(".modal");
-//       var uid = Math.round(Math.random() * 100000.0).toString();
-//       btn.attr("data-target", "." + uid);
-//       target.addClass(uid);
-//       addEscape();
-//   	});
-//   });
