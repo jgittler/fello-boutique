a3b97c218af9672298ffff86635eefe3c4feba8c
jgittler <jason.gittler@gmail.com>
2016-09-15 20:46:57 -0700

    hits and analytics, ios/safari bug fixes

--- assets/added.js
+++ assets/added.js
@@ -1,9 +1,11 @@
 function sleep(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
+  return new Promise(function (resolve) {
+    return setTimeout(resolve, time);
+  });
 }
 
 $(".quick-view").on("click", function() {
-  sleep(500).then(() => {
+  sleep(500).then(function() {
     $("#quick-view-modal").find("#size-link").remove();
     $("#quick-view-modal").find(".price").css("padding-bottom", "20px");
     $(".product-single__thumbnail").removeClass("hidden");
